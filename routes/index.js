fa54e127e043f3762a8ec7e574a134f3b3d83dee
GoodEatz <kenmytruong.94@gmail.com>
2017-11-14 18:16:03 -0800

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb')
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://GoodEatz:trooper1@ds259085.mlab.com:59085/heroku_rk6s2bvj';
+
+
+//LOAD the various controllers
+//var controllerMain = require('../controllers/main');   //this will load the main controller file
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Orders collection
+
+//MAY HAVE OTHER CODE in index.js
+
+router.get('/mongodb', function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+
+        //get collection of orders
+        var Orders = db.collection('Orders');
+
+        //get all Orders
+        Orders.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
+
+            if(err) throw err;
+
+            response.render('/getAllOrders', {results: docs});
+
+        });
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+
+    });//end of connect
+
+});//end router.get
+
+
+
+//CODE to route /getAllOrders to appropriate  Controller function
+//**************************************************************************
+//***** mongodb get all of the Orders in Orders collection w
+//      and Render information iwith an ejs view
+router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
+
+module.exports = router;
