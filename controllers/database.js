5324c64cb352396fdf7394fcaa4c7459c608ee82
GoodEatz <kenmytruong.94@gmail.com>
2017-11-27 23:08:29 -0800

    Project 2 first try

--- controllers/database.js
+++ controllers/database.js
@@ -1,41 +1,108 @@
+var express = require('express');
+var router = express.Router();
 var mongodb = require('mongodb');
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://KennyTruong:applejim@ds163745.mlab.com:63745/heroku_p4pgb3n7';
 
-module.exports.getAllOrders =  function (request, response) {
+
+var bodyParser = require('body-parser');
+var path = require('path');
+var querystring = require('querystring');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
+
+
+module.exports.storeData =  function (request, response) {
+
+    var billingName = request.body.billingName;
+    var billingStreet = request.body.billingAddress;
+    var billingCity = request.body.billingCity;
+    var billingState = request.body.billingState;
+    var billingZipCode = request.body.billingZipCode;
+    var shippingStreet = request.body.shippingAddress;
+    var shippingCity = request.body.shippingCity;
+    var shippingState = request.body.shippingState;
+    var shippingZipCode = request.body.shippingZipCode;
+    var email = request.body.email;
+    var product_vector = request.body.products;
+    var cardNumber = request.body.cardNumber;
+    var expirationDate = request.body.expirationDate;
+    var securityCode = request.body.securityCode;
+    var cardType = request.body.CCcompany;
 
     mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
         if(err) throw err;
 
+        var customerID = Math.floor((Math.random() * 1000000000000) + 1);
+        var billingID = Math.floor((Math.random() * 1000000000000) + 1);
+        var shippingID = Math.floor((Math.random() * 1000000000000) + 1);
+        var ordersID = Math.floor((Math.random() * 1000000000000) + 1);
+
         //get collection of orders
         var Orders = db.collection('ORDERS');
+        var Customers = db.collection('CUSTOMERS');
+        var Billing = db.collection('BILLING');
+        var Shipping = db.collection('SHIPPING');
 
 
-        //FIRST showing you one way of making request for ALL orders and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        var c = Orders.find({});
+        //customer collection operation
+        var customerData = {
+            _id: customerID,
+            NAME: billingName,
+            STREET: billingStreet,
+            CITY: billingCity,
+            STATE: billingState,
+            ZIP: billingZipCode,
+            EMAIL: email
+        };
 
-        c.forEach(
-            function(myDoc) {
-                console.log( "name: " + myDoc.name );  //just  loging the output to the console
-            }
-        );
-
-
-        //SECOND -show another way to make request for ALL Orders  and simply collect the  documents as an
-        //   array called docs that you  forward to the  getAllOrders.ejs view for use there
-        Orders.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('getAllOrders', {results: docs});
-
+        Customers.insertOne(customerData, function (err, result) {
+            if (err) throw err;
         });
 
+        //shipping collection operation
+        var shippingData = {
+            _id: shippingID,
+            CUSTOMER_ID: customerID,
+            SHIPPING_STREET: shippingStreet,
+            SHIPPING_CITY: shippingCity,
+            SHIPPING_STATE: shippingState,
+            SHIPPING_ZIP: shippingZipCode
+        };
 
-        //Showing in comments here some alternative read (find) requests
-        //this gets Orders where frequency>=10 and sorts by name
-        // Orders.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Orders by name
-        //  Orders.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
+        Shipping.insertOne(shippingData, function (err, result) {
+            if (err) throw err;
+        });
+
+        //billing collection operation
+        var billingData = {
+            _id: billingID,
+            CUSTOMER_ID: customerID,
+            CREDITCARDNUM: cardNumber,
+            CREDITCARDTYPE: cardType,
+            CREDITCARDEXP: expirationDate,
+            CREDITCARDSECURITYNUM: securityCode
+        };
+
+        Billing.insertOne(billingData, function (err, result) {
+            if (err) throw err;
+        });
+
+        //orders collection operation
+        var date = new Date();
+        var current_date = (date.getMonth() + date.getDate() + date.getFullYear());
+        var ordersData = {
+            _id: ordersID,
+            CUSTOMER_ID: customerID,
+            BILLING_ID: billingID,
+            SHIPPING_ID: shippingID,
+            DATE: current_date,
+            PRODUCT_VECTOR: product_vector,
+            ORDER_TOTAL: product_vector['total']
+        };
+
+        Orders.insertOne(ordersData, function (err, result) {
+            if (err) throw err;
+        });
 
 
         //close connection when your app is terminating.
--- routes/index.js
+++ routes/index.js
@@ -3,45 +3,28 @@ var router = express.Router();
 var mongodb = require('mongodb')
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://KennyTruong:applejim@ds163745.mlab.com:63745/heroku_p4pgb3n7';
 
+var bodyParser = require('body-parser');
+var path = require('path');
+var querystring = require('querystring');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: true}));
 
 //LOAD the various controllers
 //var controllerMain = require('../controllers/main');   //this will load the main controller file
-var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Orders collection
+var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb
 
 //MAY HAVE OTHER CODE in index.js
 
-router.get('/mongodb', function (request, response) {
+router.post('/storeData', function (request, response) {
 
-    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
+    //response.send(request.body);
 
-        //get collection of orders
-        var Orders = db.collection('ORDERS');
+});//end router.post
 
-        //get all Orders
-        Orders.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
-
-            if(err) throw err;
-
-            response.render('/getAllOrders', {results: docs});
-
-        });
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-
-    });//end of connect
-
-});//end router.get
-
-
-
-//CODE to route /getAllOrders to appropriate  Controller function
+//CODE to route /storeData to appropriate  Controller function
 //**************************************************************************
-//***** mongodb get all of the Orders in Orders collection w
-//      and Render information iwith an ejs view
-router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
+//***** mongodb get all of the Orders in Orders collection
+//      and Render information with an ejs view
+router.post('/storeData', controllerMongoCollection.storeData);
 
 module.exports = router;
