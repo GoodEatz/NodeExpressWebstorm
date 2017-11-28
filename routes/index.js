db925c0a3eb4c712cef6e17fa0daaba7beb01ec4
GoodEatz <kenmytruong.94@gmail.com>
2017-11-28 01:10:19 -0800

    Project 2 3rd try

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,36 @@ var router = express.Router();
 var mongodb = require('mongodb')
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://KennyTruong:applejim@ds163745.mlab.com:63745/heroku_p4pgb3n7';
 
-/*
 var bodyParser = require('body-parser');
 var path = require('path');
 var querystring = require('querystring');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
-*/
+var multer = require('multer');
+var upload = multer();
+
 //LOAD the various controllers
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb
 
 router.post('/', function (req, res) {
-    res.send('Order Successful');
+    console.log(req.body);
+    var body = JSON.stringify(req.body);
+    var params = JSON.stringify(req.params);
+    var billingName = req.body.billingName;
+    var billingStreet = req.body.billingAddress;
+    var billingCity = req.body.billingCity;
+    var billingState = req.body.billingState;
+    var billingZipCode = req.body.billingZipCode;
+    var shippingStreet = req.body.shippingAddress;
+    var shippingCity = req.body.shippingCity;
+    var shippingState = req.body.shippingState;
+    var shippingZipCode = req.body.shippingZipCode;
+    var email = req.body.email;
+    var product_vector = req.body.products;
+    var cardNumber = req.body.cardNumber;
+    var expirationDate = req.body.expirationDate;
+    var securityCode = req.body.securityCode;
+    var cardType = req.body.CCcompany;
 })
 
 //CODE to route /storeData to appropriate  Controller function
